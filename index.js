554e6a9a38ace2e8d2742a1e33f385632f766d68
Chinasa <kamogelomba@gmail.com>
2025-06-27 15:48:31 +0200

    initial project

--- index.js
+++ index.js
@@ -8,27 +8,30 @@ const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const blogs = []
 const blogHeaders = []
-// function itemPosition(array, item) {
-//        array.indexOf(item)
-// }
-const data = {
-       blog: blogs,
-       headers:blogHeaders,
-      }
+
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}))
+
+
+const data = {
+      blogs: blogs,
+      headers:blogHeaders,
+      }
+
+
+
 
 app.listen(port, ()=>{
       console.log(`server running on port ${port}`)
 })
 
 app.get("/",(req,res)=>{
-      console.log("user on")
       res.render("index.ejs")
 })
 
 app.post("/save",(req,res) =>{
+      
       function uploadBlog() {
             blogHeaders.push(req.body["heading"])
             blogs.push(req.body["blog-content"])
@@ -40,6 +43,10 @@ app.post("/save",(req,res) =>{
 })
 
 app.get("/blogs", (req,res) =>{
+      res.render("blogs.ejs", data)
+})
+app.post("/search", (req,res) =>{
+      edit = blogs[index]
       res.render("blogs.ejs", data)
 })
 
